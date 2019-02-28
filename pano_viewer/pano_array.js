d98cb811648e3b74f0a8e668860e0aa9016f0665
gogoend <gogoend@qq.com>
2019-02-28 19:28:36 +0800

    PanoViewer：为全景照片添加拍摄时的经纬度（虚拟值）

--- pano_viewer/pano_array.js
+++ pano_viewer/pano_array.js
@@ -3,66 +3,110 @@ var panos=[
         imgName:"20170101_010102_panorama2.jpg",
         imgThumb:"20170101_010102_thumb.png",
         desc:"行知楼后，夕阳西下",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:100
     },
     {
         imgName:"img_20170101_010102_panorama.jpg",
         imgThumb:"img_20170101_010102_thumb.png",
         desc:"梦溪湖之夜",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:40
     },
     {
         imgName:"img_20180424_180753_panorama22.jpg",
         imgThumb:"img_20180424_180753_thumb.png",
         desc:"梦溪湖上空100米",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:20
     },
     {
         imgName:"img_20180702_194917_panorama22.jpg",
         imgThumb:"img_20180702_194917_thumb.png",
         desc:"二食堂前",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:16
     },    
     {
         imgName:"img_20170101_010101_panorama_(2).jpg",
         imgThumb:"img_20170101_010101_thumb_(2).jpg",
         desc:"田径场·雨天",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:16
     },    
     {
         imgName:"img_20170101_010101_panorama_(3).jpg",
         imgThumb:"img_20170101_010101_thumb_(3).jpg",
         desc:"田径场荒地上空",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:16
     },    
     {
         imgName:"img_20180424_170232_panorama22.jpg",
         imgThumb:"img_20180424_170232_thumb22.jpg",
         desc:"理工一·桃花盛开时",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:16
     },    
     {
         imgName:"img_20180430_140349_panorama21.jpg",
         imgThumb:"img_20180430_140349_thumb21.jpg",
         desc:"公路大桥",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:16
     },    
     {
         imgName:"img_20180507_153147_panorama01.jpg",
         imgThumb:"img_20180507_153147_thumb01.jpg",
         desc:"师大校区，南岗",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:16
     },
         {
         imgName:"img_20180508_163510_panorama.jpg",
         imgThumb:"img_20180508_163510_thumb.jpg",
         desc:"十公寓路口",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:16
     },
         {
         imgName:"img_20180608_185528_panorama.jpg",
         imgThumb:"img_20180608_185528_thumb.jpg",
         desc:"田径场·夕阳",
+        geoCood:{
+            lng:126.55952443096922,
+            lat:45.87038861682604
+        },
         like:16
     },
 ]
