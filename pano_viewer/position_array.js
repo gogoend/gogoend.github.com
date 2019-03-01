522663a22c939e635d61ef42ddb95a5b328e9f83
gogoend <gogoend@qq.com>
2019-03-01 17:43:46 +0800

    panoViewer：将各个地点进行分离

---
+++ pano_viewer/position_array.js
@@ -0,0 +1,112 @@
+var positions = [
+    {
+        type: 'place',
+        posName: "公共卫生间",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.565688,
+            "lat": 45.876491
+        }
+    },
+    {
+        type: 'place',
+        posName: "超级巨大超市",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.565116,
+            "lat": 45.875294
+        }
+    },
+    {
+        type: 'place',
+        posName: "张漂亮酸汤鸡脚",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.563549,
+            "lat": 45.869033
+        }
+    },
+    {
+        type: 'place',
+        posName: "老奶洋芋",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.564755,
+            "lat": 45.875585
+        }
+    },
+    {
+        type: 'place',
+        posName: "田记烤肉",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.566041,
+            "lat": 45.86973
+        }
+    },
+    {
+        type: 'place',
+        posName: "赛神仙KTV",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.565265,
+            "lat": 45.875536
+        }
+    },
+    {
+        type: 'place',
+        posName: "刘嬢腌菜",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.56618,
+            "lat": 45.875941
+        }
+    },
+    {
+        type: 'place',
+        posName: "李好看咖啡",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.567973,
+            "lat": 45.875637
+        }
+    },
+    {
+        type: 'place',
+        posName: "王美丽发廊",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.563785,
+            "lat": 45.869142
+        }
+    },
+    {
+        type: 'place',
+        posName: "吴师电脑算命",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.568102,
+            "lat": 45.871185
+        }
+    },
+    {
+        type: 'place',
+        posName: "东施臭豆腐",
+        posDistance: null,
+        posAngle:null,
+        geoCood: {
+            "lng": 126.572702,
+            "lat": 45.876018
+        }
+    }
+]
